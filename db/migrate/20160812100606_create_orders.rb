b3d3678709121ffd033b52ab480fca608c25b342
Nicolas F <christiannicolasfeidner@gmail.com>
2016-08-12 12:06:44 +0200

    updated product id 4.4.

--- db/migrate/20160812100606_create_orders.rb
+++ db/migrate/20160812100606_create_orders.rb
@@ -2,10 +2,10 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.integer :user_id
-      t.integer :products_id
+      t.integer :product_id
       t.float :total
     end
     add_index :orders, :user_id
-    add_index :orders, :products_id
+    add_index :orders, :product_id
   end
 end
