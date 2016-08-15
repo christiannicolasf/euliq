56bf098d7f948602cf6bd6dc4df45036c7de5b79
Nicolas F <christiannicolasfeidner@gmail.com>
2016-08-15 17:43:23 +0200

    5.3 adding Contactforms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,15 @@ def index
   end
 end
 
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'christiannicolasfeidner@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+end
 
 
   # GET /products/1
