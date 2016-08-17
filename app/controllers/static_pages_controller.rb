f4061fe0c2953d42d2c13b6c67b51129ddd15f46
Nicolas F <christiannicolasfeidner@gmail.com>
2016-08-17 09:25:38 +0200

    5.3 fixing email problems

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,3 +8,9 @@ class StaticPagesController < ApplicationController
 
 end
 
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "europeanliqueurs@heaven.com"
+  default from: 'europeanliqueurs@heaven.com'
   layout 'mailer'
 end
